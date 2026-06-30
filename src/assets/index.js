65fe730f11245666e81c7aacea5e090d9c80fd7c
Jeslyn V <162588624+midnightwalks@users.noreply.github.com>
2026-06-30 23:11:07 +0700

    rev index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,6 +1,9 @@
 import bg from './bg.jpg'
 
-import picture from './picture.png'
+import picture1 from './picture1.jpeg'
+import picture2 from './picture2.jpeg'
+import picture3 from './picture3.jpeg'
+import picture4 from './picture4.jpeg'
 
 import confetti from './confetti.gif'
 
