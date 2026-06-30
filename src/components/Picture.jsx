1588ac93c9aff607f4a8499f116ad07f8f25a7fc
Jeslyn V <162588624+midnightwalks@users.noreply.github.com>
2026-06-30 23:01:32 +0700

    rev pics

--- src/components/Picture.jsx
+++ src/components/Picture.jsx
@@ -1,35 +1,45 @@
 import React, { useState } from 'react';
 import { motion } from 'framer-motion';
-import { picture} from '../assets';
-import { Link } from 'react-router-dom'; 
+import picture1 from '../assets/picture1.jpg';
+import picture2 from '../assets/picture2.jpg';
+import picture3 from '../assets/picture3.jpg';
+import picture4 from '../assets/picture4.jpg';
+import { Link } from 'react-router-dom';
 import SectionWrapper from './SectionWrapper';
-// Add your own images by putting them in the assets folder and import them.
-const images = [
- picture,
- picture,
- picture,
- picture,
 
+// Images to display
+const images = [
+  picture1,
+  picture2,
+  picture3,
+  picture4,
 ];
+
 function Picture() {
   const [loadedImages, setLoadedImages] = useState(0);
 
   const handleImageLoad = () => {
     setLoadedImages((prev) => prev + 1);
   };
+
   const allImagesLoaded = loadedImages === images.length;
+
   return (
     <SectionWrapper>
       <Link to="/card">
-        <p className="absolute text-4xl font-bold text-customBlue inset-0 flex justify-center items-center text-center transform rotate-6 cursor-pointer">
+        <p className="absolute inset-0 flex justify-center items-center text-center text-4xl font-bold text-customBlue transform rotate-6 cursor-pointer">
           You're Getting Old! :P
         </p>
       </Link>
+
       {!allImagesLoaded && (
         <div className="absolute inset-0 flex justify-center items-center">
-          <p className="text-xl font-medium text-gray-500">Loading images...</p>
+          <p className="text-xl font-medium text-gray-500">
+            Loading images...
+          </p>
         </div>
       )}
+
       {images.map((image, index) => (
         <motion.div
           key={index}
@@ -53,7 +63,7 @@ function Picture() {
             src={image}
             alt={`Stacked image ${index + 1}`}
             className="w-full h-full object-cover rounded-lg shadow-lg"
-            onLoad={handleImageLoad} // Increment the counter when the image loads
+            onLoad={handleImageLoad}
           />
         </motion.div>
       ))}
